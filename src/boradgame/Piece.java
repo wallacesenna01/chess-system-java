0580d21d17fe871e0b66365b2f8288e0fa21ccbd
Wallace Artur <wallacearthur_3@hotmail.com>
2024-10-13 09:16:27 -0300

    Startint to implements board and pieces

---
+++ src/boradgame/Piece.java
@@ -0,0 +1,19 @@
+package boradgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+	
+	
+}
